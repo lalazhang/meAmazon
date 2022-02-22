f6c1df87b9e2fca6db302e531a2f5d4368278e93
lalazhang <zhanglala0208@gmail.com>
2022-02-22 06:22:39 -0500

    added product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,35 @@
+import data from './data';
 
 function App() {
   return (
-    <div >
-      <header >
-       <a href="/">me Amazon</a>
+    <div>
+      <header>
+        <a href="/">me Amazon</a>
       </header>
-    <main>
-      list products
-    </main>
+      <main>
+        <h1>Featured Product</h1>
+        <div className="products">
+          {data.product.map((product) => (
+            //Each product object has an unique key value
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt="top"></img>
+              </a>
+
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                  <p>{product.brand}</p>
+                </a>
+                <p>
+                  <strong>{`CAD ${product.price}`}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
