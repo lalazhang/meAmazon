7d3c663f62af470bae7a0575292ce06a2aabe067
lalazhang <zhanglala0208@gmail.com>
2022-05-12 05:23:10 -0400

    shopping cart shows added products images and names

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -2,6 +2,9 @@ import { useContext, useState } from 'react';
 import { Store } from '../Store';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import MessageBox from '../components/MessageBox';
+import { ListGroup, ListGroupItem, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 //./means this folder, ../ means go back to parent folder
 function CartScreen() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
@@ -12,14 +15,38 @@ function CartScreen() {
   return (
     <div>
       <h1>Shopping Cart</h1>
-      <Row>
-        {cartItems.map((item) => (
-          <div>
+
+      {cartItems.length === 0 ? (
+        <MessageBox variant="secondary">
+          YOUR CART IS EMPTY
+          <Nav className="justify-content-center">
+            <Link to="/">Go Shopping</Link>
+          </Nav>
+        </MessageBox>
+      ) : (
+        <Row>
+          <ListGroup>
             {' '}
-            <Col md="3">{item.product.name}</Col>
-          </div>
-        ))}
-      </Row>
+            {cartItems.map((item) => (
+              <ListGroupItem key={item.product._id}>
+                {' '}
+                <Row>
+                  <Col md="4">
+                    <img
+                      className="img-fluid rounded img-thumbnail"
+                      src={item.product.image}
+                      alt={item.product.name}
+                    ></img>{' '}
+                    <Link to={`/product/${item.product.slug}`}>
+                      {item.product.name}
+                    </Link>
+                  </Col>
+                </Row>
+              </ListGroupItem>
+            ))}
+          </ListGroup>
+        </Row>
+      )}
     </div>
   );
 }
