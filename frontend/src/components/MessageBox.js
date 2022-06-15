304d5e8b3b5967078f54b4798a9c724ef564f648
lalazhang <zhanglala0208@gmail.com>
2022-06-15 05:06:28 -0400

    fix messageBox for cart is empty

--- frontend/src/components/MessageBox.js
+++ frontend/src/components/MessageBox.js
@@ -1,7 +1,11 @@
 import { Alert } from 'react-bootstrap';
 //Alert is not in react-bootstrap/Alert
 function MessageBox(props) {
-  return <Alert variant={props.variant}>{props.error}</Alert>;
+  return (
+    <Alert variant={props.variant}>
+      {props.error ? props.error : props.children}
+    </Alert>
+  );
 }
 
 export default MessageBox;
